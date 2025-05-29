17927b43a6c09acf8a7de0b81734314f90bb45bd
Filipe Cordeiro <filipe.cordeiro@somagrupo.com.br>
2025-05-29 11:50:26 -0300

    feat: Add logo and main styles, implement product display component, and enhance welcome section with icons
    - Added logo.svg to assets
    - Created main.css for global styles including buttons, headers, and layout
    - Implemented ProductDisplay.vue for displaying product details and variants
    - Developed TheWelcome.vue with multiple WelcomeItem components for documentation, tooling, ecosystem, community, and support
    - Added icon components for community, documentation, ecosystem, support, and tooling
    - Set up main.js to initialize Vue app and include main styles
    - Configured vite.config.js for Vue and development tools

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from "vue";
+import ProductDisplay from "@/components/ProductDisplay.vue";
+
+const cart = ref([]);
+const premium = ref(true);
+const updateCart = (id) => {
+  cart.value.push(id);
+};
+</script>
+
+<template>
+  <div class="nav-bar"></div>
+  <div class="cart">Cart({{ cart.length }})</div>
+  <ProductDisplay :premium="premium" @add-to-card="updateCart"></ProductDisplay>
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/ProductDisplay.vue
@@ -0,0 +1,80 @@
+<script setup>
+import { ref, computed } from "vue";
+import socksGreenImage from "@/assets/images/Luigi_socks.webp";
+import socksRedImage from "@/assets/images/mario_socks.avif";
+
+const props = defineProps({
+    premium: {
+        type: Boolean,
+        required: true
+    }
+})
+const emit = defineEmits(["add-to-card"]);
+const selectedVariant = ref(0);
+const product = ref("Socks");
+const brand = ref("Vue Mastery");
+const variants = ref([
+    { id: 2234, color: "green", image: socksGreenImage, quantity: 50 },
+    { id: 2235, color: "red", image: socksRedImage, quantity: 0 },
+]);
+const details = ref(["50% cotton", "30% wool", "20% polyester"]);
+const addToCard = () => {
+    emit("add-to-card", variants.value[selectedVariant.value].id);
+};
+const updateVariant = (index) => {
+  selectedVariant.value = index;
+};
+const title = computed(() => {
+  return brand.value + " " + product.value;
+});
+const image = computed(() => {
+    return variants.value[selectedVariant.value].image
+})
+const inStock = computed(() => {
+    return variants.value[selectedVariant.value].quantity > 0 
+})
+const shipping = computed(() => {
+    if (props.premium) {
+        return "Free"
+    }
+    return 2.99
+})
+</script>
+
+<template>
+    <div class="product-display">
+    <div class="product-container">
+      <div class="product-image">
+        <img :src="image" alt="Socks Green" />
+      </div>
+
+      <div class="product-info">
+        <div class="header">
+          <h1>{{ title }}</h1>
+        </div>
+        <p v-if="inStock" >In Stock</p>
+        <p v-else>Out of Stock</p>
+        <p> shipping {{ shipping }}</p>
+
+        <ul>
+          <li v-for="detail in details">{{ detail }}</li>
+        </ul>
+        <div
+          v-for="(variant, index) in variants"
+          :key="variant.id"
+          @click="updateVariant(index)"
+          class="color-circle"
+          :style="{ backgroundColor: variant.color }"
+        ></div>
+        <button
+          class="button"
+          :class="{ disabledButton: !inStock }"
+          @click="addToCard"
+          :disabled="!inStock"
+        >
+          Add to cart
+        </button>
+      </div>
+    </div>
+ </div>
+</template>
