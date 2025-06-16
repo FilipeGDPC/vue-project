72f9e396ecf90ab6b800782ba9fc766ccb6a69e1
Filipe Cordeiro <filipe.cordeiro@somagrupo.com.br>
2025-06-16 11:55:00 -0300

    feat: Add ReviewList component to display user reviews

---
+++ src/components/ReviewList.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+  reviews: {
+    type: Array,
+    required: true
+  }
+});
+</script>
+
+<template>
+    <div class="review-container">
+        <h3>Reviews:</h3>
+        <ul class="review-list">
+            <li v-for="(review, index) in reviews" :key="index">
+                <span>{{ review.name }} gave this {{review.rating}} stars</span>
+                <br/>
+                <span>{{ review.content }}</span>
+            </li>
+        </ul>
+</template>
